1f2033ceafeddbbd02214d6d3dfa6c93521e247b
Connor Monahan <cma2714@gmail.com>
2014-01-11 13:24:38 -0600

    Testing with image

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,17 @@
 #include "timer.h"
 #include <iostream>
 #include <fstream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 /// OpenCV Namespace
 using namespace cv;
 using namespace std;
 
 #define Minimum_Area 500
+#define ESC 27
+#define LOAD_IMAGE 1
+#define USE_CAMERA 0
 
 float length(float x1, float y1, float x2, float y2)
 {
@@ -66,23 +71,18 @@ void T2B_L2R(CvPoint* pt)
 int main()
 {
     /// Declare default values of trackbars
-    int hue_min = 50;
-    int saturation_min = 20;
-    int value_min = 150;
+    int hue_min = 70;
+    int saturation_min = 30;
+    int value_min = 155;
     int hue_max = 255;
     int saturation_max = 255;
     int value_max = 255;
-    int accuracy = 1;
-
-    CvPoint* pt[4];
+    const int accuracy = 2; //maximum distance between the original curve and its approximation
 
     /// Declare Timing and create variables to store frame times
     DECLARE_TIMING(FPS);
     double time_now;
     double time_ave;
-
-    /// Define character to store keypress
-    char c;
 
     /// Define character to store a string of up to 50 characters, to be printed on the image
     char str[50];
@@ -101,57 +101,64 @@ int main()
     namedWindow("HSV", CV_WINDOW_AUTOSIZE);
     namedWindow("Final", CV_WINDOW_AUTOSIZE);
 
-    while (1) /// Start a while loop where processing will occur
+    Mat inframe;
+
+    if(LOAD_IMAGE)
+    {
+        inframe = imread("raw_img.jpg");
+    }
+
+#if ( USE_CAMERA == 1 )
+    while ( 1 )
     {
         /// Start timing a frame (FPS will be a measurement of the time it takes to process all the code for each frame)
         START_TIMING(FPS);
 
-        /// Make char c equal a keypress and break if you press esc(27)
-        c = waitKey(1);
-        if(c==27)
-        {
+        char ch;
+        //Break out of loop if esc is pressed
+        if( (ch = waitKey(10) ) == ESC)
             break;
-        }
-
-        /// Accuracy should always be >10 (Will have to figure out how to relate this to contour perimeter)
-        if(accuracy < 10)
-        {
-            accuracy = 10;
-        }
 
         /// Grab a frame and contain it in the cv::Mat img
         img = cvQueryFrame( capture0 );
+        if ( ch == 's' )
+            imwrite ( "raw_img.jpg", img );
+#else
+        //changes according to camera used
+        inframe.copyTo(img);
+        {
+
 
         /// Store the original image img to the Mat dst
         img.copyTo(dst);
-
+#endif
         /// Convert img from BGR(Blue,Green,Red) to HSV(Hue,Saturation,Value)
         cvtColor(img,img,CV_BGR2HSV);
 
-        /// Read values from trackbars and "Threshold" image to pixels in the ranges
+        /// "Threshold" image to pixels in the ranges
         inRange(img,Scalar(hue_min, saturation_min, value_min), Scalar(hue_max, saturation_max, value_max),img);
 
         /// Save img to thresh Mat to show to the user
         img.copyTo(thresh);
+
+        erode(img, img, NULL);
 
         /// Declare containers for contours and contour heirarchy
         vector<vector<Point> > contours;
         vector<vector<Point> > Static_Target;
         vector<vector<Point> > Dynamic_Target;
         vector<Vec4i> hierarchy;
-        //vector<Rect> boundRect( contours.size() );
-        //vector<Point2f> point[4];
-        //vector<Point2f>center( contours.size() );
+
         findContours(img, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_NONE, Point(0, 0) );
 
         /// Declare container for approximated polygons
         vector<vector<Point> > contours_poly( contours.size() );
 
-        double Ratio = 0;
-        double Ratio_Top = 0;
-        double Ratio_Bottom = 0;
-        double Ratio_Right = 0;
-        double Ratio_Left = 0;
+        double Ratio;
+        double Ratio_Top;
+        double Ratio_Bottom;
+        double Ratio_Right;
+        double Ratio_Left;
 
         /// Create a for loop to go through each contour (i) one at a time
         //why not while(contours)?
@@ -164,23 +171,32 @@ int main()
                 approxPolyDP( contours[i], contours_poly[i], accuracy, true );
 
                 /// If 4 sided
-                if(contours_poly[i].size()==4)
+                if(contours_poly[i].size()== 4 && isContourConvex(contours_poly[i]))
                 {
-       Scalar rect_color = Scalar(255, 0, 0);
-                    drawContours(dst, contours_poly,i, rect_color, 3, 8, hierarchy, 0, Point() );
 
-                    /// get corners from approxpoly
+                    //drawContours(dst, contours_poly,i, Scalar(255,0,0), 3, 8, hierarchy, 0, Point() );
 
-                    //populate the 4 corners in pt[4]
+                    for ( int j = 0; j < 4; j++ )
+                    {
+                        if ( contours_poly[i][j].x < contours_poly[i][0].x )
+                            swap ( contours_poly[i][0], contours_poly[i][j] );
+                        else
+                            if ( contours_poly[i][j].y < contours_poly[i][1].y )
+                                swap ( contours_poly[i][1], contours_poly[i][j] );
+                            else
+                                if ( contours_poly[i][j].x > contours_poly[i][2].x )
+                                    swap ( contours_poly[i][2], contours_poly[i][j] );
+                                else
+                                    if ( contours_poly[i][j].y > contours_poly[i][3].y )
+                                        swap ( contours_poly[i][3], contours_poly[i][j] );
+                    }
 
-                    /// Order corners top to bottom left to right
-                    //T2B_L2R(CvPoint* pt)
 
                     /// test aspect ratio
-//                    Ratio_Top = length(pt[0]->x, pt[0]->y, pt[1]->x, pt[1]->y);
-//                    Ratio_Bottom = length(pt[2]->x, pt[2]->y, pt[3]->x, pt[3]->y);
-//                    Ratio_Left = length(pt[0]->x, pt[0]->y, pt[2]->x, pt[2]->y);
-//                    Ratio_Right = length(pt[1]->x, pt[1]->y, pt[3]->x, pt[3]->y);
+                    Ratio_Top = length(contours_poly[i][0].x, contours_poly[i][0].y, contours_poly[i][1].x, contours_poly[i][1].y);
+                    Ratio_Bottom = length(contours_poly[i][2].x, contours_poly[i][2].y, contours_poly[i][3].x, contours_poly[i][3].y);
+                    Ratio_Left = length(contours_poly[i][0].x, contours_poly[i][0].y, contours_poly[i][2].x, contours_poly[i][2].y);
+                    Ratio_Right = length(contours_poly[i][1].x, contours_poly[i][1].y, contours_poly[i][3].x, contours_poly[i][3].y);
 
                     if ( (Ratio_Left + Ratio_Right) < 10.0) //not a target
                     {
@@ -190,28 +206,35 @@ int main()
                     {
                         Ratio = (Ratio_Left + Ratio_Right)/(Ratio_Top + Ratio_Bottom);
                     }
+
+                    sprintf(str, "Ratio = %lf", Ratio);
+                    putText(dst, str,Point(5,50), CV_FONT_HERSHEY_COMPLEX_SMALL, 0.75, Scalar(255,0,255),1,8,false);
+
                     //what if we see all 4?
-                    if (contours.size() != 3)
-                    {
-                        if (contours.size() < 4)
-                        {
+//                    if (contours.size() != 3)
+//                    {
+//                        if (contours.size() < 4)
+//                        {
                             //case 1 or 2
-                            if (Ratio > 2) //subject to change
+                            if (Ratio > 1) //subject to change
                             {
                                 //contour is a tall, skinny one
                                 //save off as stationary target
                                 Static_Target.push_back(contours[i]);
+                                drawContours(dst, contours_poly,i, Scalar(0,0,255), 3, 8, hierarchy, 0, Point() );
+
                             }
                             else
                             {
                                 //contour is the short, long, dynamic target
                                 //save off as dynamic target
                                 Dynamic_Target.push_back(contours[i]);
-                            }
-                        }
-                    }
+                                drawContours(dst, contours_poly,i, Scalar(255, 0, 0), 3, 8, hierarchy, 0, Point() );
 
-                    // Order targets left to right
+                            }
+
+
+                    /// Order targets left to right
 
                 }
 
@@ -245,6 +268,16 @@ int main()
         {
             Mass_Center_Dynamic[i] = Point2f( Moment_Center_Dynamic[i].m10/Moment_Center_Dynamic[i].m00 , Moment_Center_Dynamic[i].m01/Moment_Center_Dynamic[i].m00 );
         }
+        if(Mass_Center_Static.size() > 0 && Mass_Center_Dynamic.size() > 0
+                && Mass_Center_Static[0].x > Mass_Center_Dynamic[0].x)
+        {
+            //case left
+        }
+
+        else
+        {
+            //case right
+        }
 
         /// Stop timing and calculate FPS and Average FPS
         STOP_TIMING(FPS);
@@ -255,10 +288,15 @@ int main()
         sprintf(str, "Average FPS = %.f", time_ave);
         putText(dst, str,Point(5,30), CV_FONT_HERSHEY_COMPLEX_SMALL, 0.75, Scalar(255,0,255),1,8,false);
 
+
         /// Show Images
         imshow("Raw", img);
         imshow("HSV", thresh);
         imshow("Final", dst);
+
+#if ( USE_CAMERA == 0 )
+        waitKey();
+#endif
 
     } /// <---- End of While Loop (ESC has to be pressed to break out of loop) otherwise loop
 
